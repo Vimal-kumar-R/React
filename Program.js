cca30f17b60fe8899d779c8071c60c9e4a4532f9
Vimal-kumar-R <143787874+Vimal-kumar-R@users.noreply.github.com>
2024-05-17 02:37:14 -0800

    Add files via upload

---
+++ Program.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import './Program.css';
+export default function Program() {
+  return (
+    <div>
+         <div>
+        <div className="main">
+          <div className="container">
+            <h1 className="heading">
+              Full Stack Development & <br></br>Software Testing
+            </h1>
+            <p className="para">-Front-end or Back-end-Master It ALL.</p>
+            <p className="para">-Online Course</p>
+            <div className="duration">
+              <table>
+                <tr>
+                  <th className="box-content">6</th>
+                  <th className="box-content">8</th>
+                  <th className="box-content">10</th>
+                </tr>
+                <tr>
+                  <td className="box-content">Months</td>
+                  <td className="box-content">Modules</td>
+                  <td className="box-content">Guaranteed Interviews</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+          <div className="pic">
+            <img
+              src="https://www.visaplace.com/wp-content/uploads/2018/11/international-student-mobility.jpg"
+              width="100%"
+              height="70%"
+            ></img>
+          </div>
+        </div>
+
+        <div className="success-heading-main">
+          <h1 className="success-header">Success Stories to inspire yours</h1>
+          <marquee direction="left">
+            <div className="success-head">
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAzL2ZyZWVpbWFnZXNjb21wYW55X3Bob3RvX29mX3lvdW5nX2luZGlhbl9naXJsX2hvbGRpbmdfc3R1ZGVudF9iYV8zN2QyNjU4Yi0yOWIwLTQyZmQtODhmYy04OGU3ZTcxYmVlNDcucG5n.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Dharnya</h5>
+                <p className="stories-para">Zehoo</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTEyL3Jhd3BpeGVsX29mZmljZV80M19waG90b19vZl95b3VuZ19pbmRpYW5fbWFuX3dpdGhfc3R1ZGVudF9iYWNrcF83Y2I0ZDBlOS1kNDk3LTQ1M2YtYjBlOS05ZWRkNDkyMTQ2NTAucG5n.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Dharun</h5>
+                <p className="stories-para">TCS</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8kSAGdP8sjtbaDNH0SBDdi-SKSaMDjPk6-80gykaJfa80a5Z_6_pzRY_OG7TsQ66mckI&usqp=CAU"
+                  width="50px"
+                />
+                <h5 className="stories-head">Amarason</h5>
+                <p className="stories-para">Wipro</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://static.vecteezy.com/system/resources/thumbnails/024/724/632/small_2x/a-happy-smiling-young-college-student-with-a-book-in-hand-isolated-on-a-transparent-background-generative-ai-png.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Vimal</h5>
+                <p className="stories-para">Infosys</p>
+              </div>
+              {/* <div className="success-stories">
+               <img className="stories-pic" src="https://c8.alamy.com/comp/DJAB15/college-boy-holding-books-with-blurred-students-in-park-DJAB15.jpg" width="50px"/>
+               <h5 className="stories-head">Bharath</h5>
+               <p className="stories-para">Amazon</p>
+        </div> */}
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://static.vecteezy.com/system/resources/previews/024/724/631/original/a-happy-smiling-young-college-student-with-a-book-in-hand-isolated-on-a-transparent-background-generative-ai-free-png.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Jeniston</h5>
+                <p className="stories-para">MicroSoft</p>
+              </div>
+            </div>
+          </marquee>
+
+          <marquee direction="right">
+            <div className="success-head">
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAzL2ZyZWVpbWFnZXNjb21wYW55X3Bob3RvX29mX3lvdW5nX2luZGlhbl9naXJsX2hvbGRpbmdfc3R1ZGVudF9iYV8zN2QyNjU4Yi0yOWIwLTQyZmQtODhmYy04OGU3ZTcxYmVlNDcucG5n.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Dharnya</h5>
+                <p className="stories-para">Zehoo</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTEyL3Jhd3BpeGVsX29mZmljZV80M19waG90b19vZl95b3VuZ19pbmRpYW5fbWFuX3dpdGhfc3R1ZGVudF9iYWNrcF83Y2I0ZDBlOS1kNDk3LTQ1M2YtYjBlOS05ZWRkNDkyMTQ2NTAucG5n.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Dharun</h5>
+                <p className="stories-para">TCS</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8kSAGdP8sjtbaDNH0SBDdi-SKSaMDjPk6-80gykaJfa80a5Z_6_pzRY_OG7TsQ66mckI&usqp=CAU"
+                  width="50px"
+                />
+                <h5 className="stories-head">Amarason</h5>
+                <p className="stories-para">Wipro</p>
+              </div>
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://static.vecteezy.com/system/resources/thumbnails/024/724/632/small_2x/a-happy-smiling-young-college-student-with-a-book-in-hand-isolated-on-a-transparent-background-generative-ai-png.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Vimal</h5>
+                <p className="stories-para">Infosys</p>
+              </div>
+              {/* <div className="success-stories">
+               <img className="stories-pic" src="https://c8.alamy.com/comp/DJAB15/college-boy-holding-books-with-blurred-students-in-park-DJAB15.jpg" width="50px"/>
+               <h5 className="stories-head">Bharath</h5>
+               <p className="stories-para">Amazon</p>
+        </div> */}
+              <div className="success-stories">
+                <img
+                  className="stories-pic"
+                  src="https://static.vecteezy.com/system/resources/previews/024/724/631/original/a-happy-smiling-young-college-student-with-a-book-in-hand-isolated-on-a-transparent-background-generative-ai-free-png.png"
+                  width="50px"
+                />
+                <h5 className="stories-head">Jeniston</h5>
+                <p className="stories-para">MicroSoft</p>
+              </div>
+            </div>
+          </marquee>
+        </div>
+
+        <div className="syllabus">
+          <h1 className="syllabus-heading">Take A Look At Our Syllabus</h1>
+          <div className="table-head">
+          <table>
+            <tr>
+              <th>
+              <div className="module">Module 1 :HTML</div>
+              </th>
+              <th>
+              <div className="module">Module 5 :MySQL</div>
+              </th>
+            </tr>
+            <tr>
+              <th>
+              <div className="module">Module 2 :Css</div>
+              </th>
+              <th>
+              <div className="module">Module 6 :MongoDB</div>
+              </th>
+            </tr>
+            <tr>
+              <th>
+              <div className="module">Module 3 :Bootstrap</div>
+              </th>
+              <th>
+              <div className="module">Module 7 :React JS</div>
+              </th>
+            </tr>
+            <tr>
+              <th>
+              <div className="module">Module 4 :JavaScript</div>
+              </th>
+              <th>
+              <div className="module">Module 8:Node JS</div>
+              </th>
+            </tr>
+          </table>
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
